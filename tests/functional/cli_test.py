3244334e12a8b1a6a44f4548343b2056e3c4604a
Andreas Lutro <anlutro@gmail.com>
2019-10-25 20:48:10 +0200

    ci and black

--- tests/functional/cli_test.py
+++ tests/functional/cli_test.py
@@ -1,18 +1,20 @@
 import subprocess
-import textwrap
+
+
+test_cli_expected = """
+digraph {
+    "pycodegraph.analysis";
+    "pycodegraph.cli";
+    "pycodegraph.renderers";
+    "tests.unit";
+    "pycodegraph.cli" -> "pycodegraph.analysis";
+    "pycodegraph.renderers" -> "pycodegraph.analysis";
+    "tests.unit" -> "pycodegraph.analysis";
+    "tests.unit" -> "pycodegraph.renderers";
+}
+"""
 
 
 def test_cli():
-	out = subprocess.check_output(['pycodegraph', 'imports', '--depth=1'])
-	assert out.decode().strip() == textwrap.dedent('''
-	digraph {
-	    "pycodegraph.analysis";
-	    "pycodegraph.cli";
-	    "pycodegraph.renderers";
-	    "tests.unit";
-	    "pycodegraph.cli" -> "pycodegraph.analysis";
-	    "pycodegraph.renderers" -> "pycodegraph.analysis";
-	    "tests.unit" -> "pycodegraph.analysis";
-	    "tests.unit" -> "pycodegraph.renderers";
-	}
-	''').strip()
+    out = subprocess.check_output(["pycodegraph", "imports", "--depth=1"])
+    assert out.decode().strip() == test_cli_expected.strip()
