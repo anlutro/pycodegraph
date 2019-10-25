1c8d596d8ab2dfdf60f6c0f68410f5020d027b03
Andreas Lutro <anlutro@gmail.com>
2019-10-25 20:57:47 +0200

    update setup.py, add black

--- setup.py
+++ setup.py
@@ -5,12 +5,15 @@ import setuptools
 
 setuptools.setup(
     name="pycodegraph",
-    packages=setuptools.find_packages(include=("pycodegraph", "pycodegraph.*")),
     version="0.1",
     license="MIT",
     description="Analyze and make graphs from Python code.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Andreas Lutro",
     author_email="anlutro@gmail.com",
+    url="https://github.com/anlutro/pycodegraph",
+    packages=setuptools.find_packages(include=("pycodegraph", "pycodegraph.*")),
     install_requires=["allib >= 1.0, < 1.2"],
     entry_points={"console_scripts": ["pycodegraph=pycodegraph.cli:main"]},
 )
